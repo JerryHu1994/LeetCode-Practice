390a4192c92dbb0fe27ef95822dfc330e6544d11
Jieru Hu <hjr01211@gmail.com>
2019-02-03 18:53:14 -0800

    update

--- Scripts/rename.py
+++ Scripts/rename.py
@@ -3,8 +3,10 @@
 import os
 
 prepath = "../Solutions/"
+blacklist = [".DS_Store", "-DS_Store"]
 
 for file in os.listdir(prepath):
+	if file in blacklist:	continue
 	new_name = file.replace(" ", "-").replace(".", "-")
 	#print os.path.join(prepath, file), os.path.join(prepath, new_name)
 	os.rename(os.path.join(prepath, file), os.path.join(prepath, new_name))
