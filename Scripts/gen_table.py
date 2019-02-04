9d4cc4f4544a65e6fa600ef645b7b16e9d1615e3
Jieru Hu <hjr01211@gmail.com>
2019-02-03 18:10:10 -0800

    update the script

--- Scripts/gen_table.py
+++ Scripts/gen_table.py
@@ -48,14 +48,15 @@ def gen_table(sol_dir, lc_name, abs_path):
 	"generate the table for display on RAEDME"
 	finished_problems = get_finished_problems(sol_dir)
 	lc_json = read_json(lc_name)
-	outputs = ['| # | Problem | Accept Rate | Difficulty | Solutions |', '| --- | --- | --- | --- | --- |']
-	easy_cnt, medium_cnt, hard_cnt = 0, 0, 0
+	table = ['| # | Problem | Accept Rate | Difficulty | Solutions |', '| --- | --- | --- | --- | --- |']
+	cnt = collections.defaultdict(int)
 	for problem in finished_problems:
 		if problem in blacklist:	continue
 		problem_idx = problem.split("-")[0].lstrip(" ").rstrip(" ")
 		problem_title = lc_json[problem_idx]['title']
 		problem_url = lc_json[problem_idx]['url']
 		problem_difficulty = lc_json[problem_idx]['difficulty']
+		cnt[problem_difficulty] += 1
 		problem_ac_rate = '{:.0f}%'.format(lc_json[problem_idx]['accept_rate'] * 100)
 		problem_name = "[{}]({})".format(problem_title, problem_url)
 		pre_path = sol_dir+problem
@@ -66,8 +67,10 @@ def gen_table(sol_dir, lc_name, abs_path):
 			solution_with_link.append("[{}]({})".format(sol_name, abs_path+problem+"/"+sol_name))
 		problem_sol_link = ' <br> '.join(solution_with_link)
 		content = [problem_idx, problem_name, problem_ac_rate, problem_difficulty, problem_sol_link]
-		outputs.append("|"+"|".join(content)+"|")
-	return outputs
+		table.append("|"+"|".join(content)+"|")
+	lc_summary = "This repositoy contains my solutions to Leetcode algorithm problem. Currently I finished {} problems in total: {} Easy, {} Medium and {} Hard.".format(len(finished_problems), cnt['Easy'], cnt['Medium'], cnt['Hard'])
+	script_summary = "Python scripts are included to crawl the problem info from [Leetcode](https://leetcode.com/), summarize the problems into a table and output the REAMME.md."
+	return [lc_summary, script_summary] + table
 
 if __name__ == "__main__":
 	# print get_finished_problems("../Solutions")
